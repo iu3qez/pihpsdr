1ddca97b0e3d3276064000a75a972b08f1e4603d
c vw <dl1ycf@darc.de>
2025-09-12 12:35:36 +0200

    Update of the manual (included secton on the "Sliders" menu).

--- src/rx_menu.c
+++ src/rx_menu.c
@@ -228,7 +228,7 @@ void rx_menu(GtkWidget *parent) {
   GtkWidget *content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
   GtkWidget *grid = gtk_grid_new();
   gtk_grid_set_column_spacing (GTK_GRID(grid), 10);
-  gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+  gtk_grid_set_row_homogeneous(GTK_GRID(grid), FALSE);
   gtk_grid_set_column_homogeneous(GTK_GRID(grid), FALSE);
   GtkWidget *close_b = gtk_button_new_with_label("Close");
   gtk_widget_set_name(close_b, "close_button");
