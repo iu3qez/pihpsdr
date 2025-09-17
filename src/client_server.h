63255af9d3b2d274aabf5107a5b91b01176c9191
c vw <dl1ycf@darc.de>
2025-09-17 16:18:31 +0200

    Advanced client/server version

--- src/client_server.h
+++ src/client_server.h
@@ -140,7 +140,7 @@ enum _header_type_enum {
   CLIENT_SERVER_COMMANDS,
 };
 
-#define CLIENT_SERVER_VERSION 0x01000019 // 32-bit version number
+#define CLIENT_SERVER_VERSION 0x01250001 // 32-bit version number
 #define SPECTRUM_DATA_SIZE 4096          // Maximum width of a panadapter
 #define AUDIO_DATA_SIZE 512              // 512 stereo samples
 
